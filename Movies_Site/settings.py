2411f6d0ca6e0a473ec314a5eb1e008ebb448534
myselfims <shaikhimran7585@gmail.com>
2023-01-15 02:11:23 +0530

    os update

--- Movies_Site/settings.py
+++ Movies_Site/settings.py
@@ -144,7 +144,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # )
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 
 # Default primary key field type
