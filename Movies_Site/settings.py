0eb080c56344ed90cd4448eb135c7e23ee127259
myselfims <shaikhimran7585@gmail.com>
2023-01-15 02:27:43 +0530

    static debug

--- Movies_Site/settings.py
+++ Movies_Site/settings.py
@@ -140,10 +140,10 @@ STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static/')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static/')
+# ]
 
 
 # Default primary key field type
