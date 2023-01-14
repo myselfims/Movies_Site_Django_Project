4ff1dd97d2cd638fd3d0834f1563f4b8d8fd18d7
myselfims <shaikhimran7585@gmail.com>
2023-01-15 03:30:33 +0530

    Static Files Added

--- Movies_Site/settings.py
+++ Movies_Site/settings.py
@@ -137,12 +137,12 @@ STATIC_URL = 'static/'
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR / "static")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
