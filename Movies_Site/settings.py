e337269c2ff427fe8eadfbbb43fe6efd008ab2cd
myselfims <shaikhimran7585@gmail.com>
2023-01-15 16:20:35 +0530

    static file changed

--- Movies_Site/settings.py
+++ Movies_Site/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR / "static")
+STATIC_ROOT = os.path.join(BASE_DIR / "static")
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
@@ -144,7 +144,7 @@ STATIC_URL = 'static/'
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
