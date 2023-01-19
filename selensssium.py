c6025423daac1c57facb3ad3c4bc507aeda94da2
myselfims <shaikhimran7585@gmail.com>
2023-01-19 18:49:49 +0530

    downlinks update

--- selensssium.py
+++ selensssium.py
@@ -2,13 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 url  = 'https://www.1377x.to/search/bahubali/1/'
-headers={"User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"}
-r = requests.get(url,headers=headers)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.2840.71 Safari/539.36'}
+# headers={"User-Agent": "Mozilla/5.0"}
+r = requests.get(url,headers=headers,timeout=15)
 soup = BeautifulSoup(r.content,'html.parser')
 anchors = soup.find_all('a',class_='torrentdown1')
 print(anchors)
-for a in anchors:
-    if 'bahubali' in str(a.get('href')).lower():
-        r = requests.get(f'https://www.1377x.to/{a.get("href")}',headers=headers)
-        print(r.content)
-        break
+# for a in anchors:
+#     if 'bahubali' in str(a.get('href')).lower():
+#         r = requests.get(f'https://www.1377x.to/{a.get("href")}',headers=headers)
+#         print(r.content)
+        # break
