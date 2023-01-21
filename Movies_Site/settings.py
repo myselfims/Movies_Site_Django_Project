4e998a3efcb8a272608e7263bc7105b70df3a7ea
myselfims <shaikhimran7585@gmail.com>
2023-01-22 00:48:13 +0530

    prepared for ubuntu hosting

--- Movies_Site/settings.py
+++ Movies_Site/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-)ska^2bjbu7gwym^9%fg&&mjp$v_@2)t9f*3e%2cgk*=s53*@1'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-)ska^2bjbu7gwym^9%fg&&mjp$v_@2)t9f*3e%2cgk*=s53*@1'
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-url = 'https://moviesjunction.onrender.com/'
+url = 'http://18.236.235.180/'
 
 STATIC_URL = url + 'static/' # For Production
 # STATIC_URL = 'static/' # For development
